980df76605e8b3c8b9a2d03b066ccaedc958a067
Professor Lozano <prof-luiz@outlook.com>
2024-04-09 10:18:54 -0300

    criação do servidor do projeto

---
+++ projeto_SMP/app.js
@@ -0,0 +1,18 @@
+//fazer a importação do express
+const express = require('express');
+
+//rotas (quando o servidor for acessado, pra onde ele vai???)
+const router = express.Router();
+router.get('/',(req,res)=>{
+    res.send('Olá turma de ADS de SMP da manhã!!');
+});
+
+//configurações básicas do aplicativo
+const app = express();
+app.use('/',router);
+
+module.exports = app; //exportando o app, pois iremos importa-lo no servidor
+
+
+
+
