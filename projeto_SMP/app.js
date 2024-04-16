e727ec98be5c98fe14a943806dfacb909a231b3c
Professor Lozano <prof-luiz@outlook.com>
2024-04-16 09:43:57 -0300

    requisição e resposta

--- projeto_SMP/app.js
+++ projeto_SMP/app.js
@@ -1,11 +1,6 @@
 //fazer a importação do express
 const express = require('express');
-
-//rotas (quando o servidor for acessado, pra onde ele vai???)
-const router = express.Router();
-router.get('/',(req,res)=>{
-    res.send('Olá turma de ADS de SMP da manhã!!');
-});
+const router = require('./routes/index');
 
 //configurações básicas do aplicativo
 const app = express();
