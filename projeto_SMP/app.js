4ef7b1019041fabf78ae20c6ff3e7a2334e9fb0d
Professor Lozano <prof-luiz@outlook.com>
2024-04-16 10:27:04 -0300

    codigo final da aula 10

--- projeto_SMP/app.js
+++ projeto_SMP/app.js
@@ -6,6 +6,8 @@ const router = require('./routes/index');
 const app = express();
 app.use('/',router);
 
+app.use(express.json());
+
 module.exports = app; //exportando o app, pois iremos importa-lo no servidor
 
 
--- projeto_SMP/routes/index.js
+++ projeto_SMP/routes/index.js
@@ -12,4 +12,22 @@ router.get('/sobre',(req,res)=>{
     res.send('Página sobre do projeto');
 });
 
+router.get('/posts/:id',(req,res)=>{
+    let id = req.params.id;
+    res.send('ID do post ' + id);
+});
+
+router.get('/json',(req,res)=>{
+    //res.send('Olá turma de ADS de SMP da manhã!!');
+    let nome = req.query.nome;
+    let sobrenome = req.query.sobrenome;
+    res.json({
+        nomeCompleto: nome+ ' ' +sobrenome
+    });
+});
+
+router.get('/json2',(req,res)=>{
+    res.json(req.query);
+});
+
 module.exports = router;
